4f9772e74fbf5a351de230dd2d6b38c1a25538a3
Gabriel Thom <gabriel.thom02@gmail.com>
2020-03-17 11:06:23 -0300

    added bitmex wstest data fields functions

--- ws_test.py
+++ ws_test.py
@@ -6,6 +6,7 @@ import json
 from decimal import Decimal
 
 
+
 # Basic use of websocket.
 def run():
     logger = setup_logger()
@@ -31,13 +32,15 @@ def run():
         # ---------------------------------------------------Bitmex Orderbook----------------------------------------------------------------------------------------
         # -----------------------------------------------------------------------------------------------------------------------------------------------------------
         # -----------------------------------------------------------------------------------------------------------------------------------------------------------
-        orderbook_data = ws.market_depth()
+        orderbook_data = ws.data
         
         # with open ('orderbook_data', 'w') as json_file:
         #     json.dump(orderbook_data, json_file)
-
-        print(get_maxprice_bids(orderbook_data))
+    
+        
+        print(get_trade_price(orderbook_data))
         sleep(2)
+        
         
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -45,33 +48,52 @@ def run():
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_last_orderid(orderbook_data):
-    last_orderid = [order['id'] for order in orderbook_data]
+    last_orderid = [order['id'] for order in orderbook_data["orderBookL2"]]
     return last_orderid[-1]
 
-def get_minprice_asks(orderbook_data):
-    order_asks = [order for order in orderbook_data if order['side']  == 'Sell']
+def get_price_asks(orderbook_data):
+    order_asks = [order for order in orderbook_data["orderBookL2"] if order['side']  == 'Sell']
     price_asks = [order['price'] for order in order_asks]
     return min(price_asks)
 
 def get_largest_ask(orderbook_data):
-    order_asks = [order for order in orderbook_data if order['side']  == 'Sell']
+    order_asks = [order for order in orderbook_data["orderBookL2"] if order['side']  == 'Sell']
     largest_ask = [order['size'] for order in order_asks]
     return max(largest_ask)
 
 def get_largest_bid(orderbook_data):
-    order_bids = [order for order in orderbook_data if order['side']  == 'Buy']
-    largest_bid = [order['size'] for order in order_bids]
-    return max(largest_bid)
+    order_bids = [order for order in orderbook_data["orderBookL2"] if order['side']  == 'Buy']
+    size_bids = [order['size'] for order in order_bids]
+    return max(size_bids)
 
-# -----------------------------------------------------------------------------------------------------------------------------------------------------------
-# ---------------------------------------------------Bitmex Actual Price-------------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------------------------------------------------------------
-def get_maxprice_bids(orderbook_data):
-    order_bids = [order for order in orderbook_data if order['side']  == 'Buy']
+def get_trade_price(orderbook_data):
+    data_trade = [order['price'] for order in orderbook_data['trade']]
+    return data_trade[-1]
+
+def get_volume(orderbook_data):
+    volume = orderbook_data['instrument'][0]['volume']
+    return volume
+
+def get_price_bids(orderbook_data):
+    order_bids = [order for order in orderbook_data["orderBookL2"] if order['side']  == 'Buy']
     price_bids = [order['price'] for order in order_bids]
     return max(price_bids)
     
+def get_price_asks2(orderbook_data):
+    ask_prices =  orderbook_data['quote'][-1]['askPrice']
+    return ask_prices   
+
+def get_volume24h(orderbook_data):
+    volume24h = orderbook_data['instrument'][0]['volume24h']
+    return volume24h
+
+def get_prevprice24h(orderbook_data):
+    prevprice24h = orderbook_data['instrument'][0]['prevPrice24h']
+    return prevprice24h
+    
+
+
+
 
 
 def setup_logger():
