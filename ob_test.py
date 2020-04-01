73e4db351f0ac79884bcbd2d580413f058cb8112
canokaue <kauecano@gmail.com>
2020-04-01 14:47:07 -0300

    .json debugging of functions

--- ob_test.py
+++ ob_test.py
@@ -5,8 +5,8 @@ from time import sleep
 # Simplejson allows serialization of Decimal
 import simplejson as json
 import sys
-from decimal import Decimal
 
+DATA_DIR = 'data/'
 
 
 # Basic use of websocket.
@@ -22,69 +22,80 @@ def run():
     # Run forever
     while(ws.ws.sock.connected):
        
-        # sleep(2)
+        sleep(2)
 
-        # Get all asks orders from orderbook. Working!
+        # Get all asks orders from orderbook.
         rbAskOrders = ws.get_aks_orders()
         logger.info('All asks orders from orderbook: %s', rbAskOrders)
-        with open('rbAskOrders.json', 'w') as f:
+        with open(DATA_DIR + 'rbAskOrders.json', 'w') as f:
             json.dump(rbAskOrders, f)
+        with open(DATA_DIR + 'len_rbAskOrders.json', 'w') as f:
+            json.dump(len(rbAskOrders), f)
+        obAskOrders = ws.get_aks_orders(fromTree=False)
+        logger.info('All asks orders from orderbook: %s', obAskOrders)
+        with open(DATA_DIR + 'obAskOrders.json', 'w') as f:
+            json.dump(obAskOrders, f)
+        with open(DATA_DIR + 'len_obAskOrders.json', 'w') as f:
+            json.dump(len(obAskOrders), f)
+
+        # Stop execution
+        # sys.exit()
 
         # When fromTree = True, get last ask price from the Tree, need fix to become the last of the orderbook. Need fix!
         rbLastAskPrice = ws.get_ask_price()
         logger.info('The last ask price : %s', rbLastAskPrice)
-        with open('rbLastAskPrice.json', 'w') as f:
+        with open(DATA_DIR + 'rbLastAskPrice.json', 'w') as f:
             json.dump(rbLastAskPrice, f)
 
-        sys.exit()
 
-        # Get the largest ask order size from the orderbook. Working!
+
+        # Get the largest ask order size from the orderbook.
         logger.info('The largest ask order size from the orderbook : %s', ws.get_ask_largest_size())
         
-        # Get all asks order sizes from orderbook. Working!
+        # Get all asks order sizes from orderbook.
         logger.info('All asks order sizes from orderbook : %s', ws.get_aks_sizes())
         
-        # Get all asks prices from orderbook. Working!
+        # Get all asks prices from orderbook.
         logger.info('All asks prices from orderbook : %s', ws.get_ask_prices())
         
-        # Get all bids orders from orderbook. Working!
+        # Get all bids orders from orderbook.
         #logger.info('All bids orders from oderbook: %s', ws.get_bid_orders())
 
         # When pull bid data without sleep(), an error occurs saying that the Tree is empty.
         # When fromTree = True, get max bid price from the Tree, need fix to become the max of the orderbook.  Need fix!
         #logger.info('The max bid price : %s', ws.get_bid_price())
 
-        # Get the largest bid order size from the orderbook.Working!
+        # Get the largest bid order size from the orderbook
         #logger.info('The largest bid order size from the orderbook : %s', ws.get_bid_largestsize())
         
-        # Get all bids order sizes from orderbook.Working!
+        # Get all bids order sizes from orderbook
         #logger.info('All bids order sizes from orderbook : %s', ws.get_bid_sizes())
         
-        # Get all bids prices from orderbook.Working!
+        # Get all bids prices from orderbook
         #logger.info('All bids prices from orderbook : %s', ws.get_bid_prices())
 
-        # Get all bids and asks data from orderbook.Working!
+        # Get all bids and asks data from orderbook
         # logger.info('Current orderbook : %s', ws.get_current_book())
 
-        # Get the instrument data from Bitmex orderbook. Working!
+        # Get the instrument data from Bitmex orderbook.
         #logger.info('Instrument data: %s', ws.get_instrument())
 
-        # Return a ticker object. Generated from quote and trade. Working!
+        # Return a ticker object. Generated from quote and trade.
         #logger.info('Ticker : %s', ws.get_ticker())
 
-        # Get whole orderbook from Bitmex. Returns all levels. Working!
+        # Get whole orderbook from Bitmex. Returns all levels.
         #logger.info('orderbook : %s', ws.market_depth())
 
-        # Get Actual trade price from Bitmex orderbook. Working!
+        # Get Actual trade price from Bitmex orderbook.
         #logger.info('Actual trade price : %s', ws.get_trade_price())
 
-        # Get volume from Bitmex orderbook. Working!
+        # Get volume from Bitmex orderbook.
         #logger.info('Bitmex volume : %s', ws.get_volume())
 
-        # Get volume 24h from Bitmex orderbook. Working!
+        # Get volume 24h from Bitmex orderbook.
         #logger.info('Bitmex volume 24h: %s', ws.get_volume24h())
 
-        # Get prevprice 24h from Bitmex orderbook. Working!
+        # Get prevprice 24h from Bitmex orderbook.
         #logger.info('Bitmex prevprice 24h: %s', ws.get_prevprice24h())
         
 def setup_logger():
